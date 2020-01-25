9cedbf923c24105d363e8b81f7b8949ac000bf84
Tammy Lam <lamtammy1515@gmail.com>
2020-01-25 04:24:16 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -7,3 +7,12 @@ end
 
 
 
+cookies_in_bag = 50 
+
+2.times do 
+  puts "I ate 2 cookies in bag already."
+  
+  cookies_in_bag = cookies_in_bag - 5
+end 
+
+puts "There are #{cookies_in_bag} cookies left in my bag."
